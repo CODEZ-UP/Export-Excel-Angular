fe2cfd261de1569eed4ce20c2dfae780feed012d
codezup <codezup@gmail.com>
2020-05-30 00:18:00 +0530

    export table data in excel code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
+import { ExcelService } from './services/excel.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'export-excel-functionality-in-angular';
+  title = 'exportExcelInAngular';
+  dataOfFootballers: any = [{
+    playerName: 'Cristiano Ronaldo',
+    playerCountry: 'Pourtgal',
+    playerClub: 'Juventus'
+  },
+  {
+    playerName: 'Lionel Messi',
+    playerCountry: 'Argentina',
+    playerClub: 'Barcelona'
+  },
+  {
+    playerName: 'Neymar Junior',
+    playerCountry: 'Brazil',
+    playerClub: 'PSG'
+  },
+  {
+  playerName: 'Tonni Kroos',
+  playerCountry: 'Germany',
+  playerClub: 'Real Madrid'
+  },
+  {
+    playerName: 'Paul Pogba',
+    playerCountry: 'France',
+    playerClub: 'Manchester United'
+  }];
+  constructor(private excelService:ExcelService){
+
+  }
+  exportAsXLSX():void {
+    this.excelService.exportAsExcelFile(this.dataOfFootballers, 'footballer_data');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { ExcelService } from './services/excel.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ExcelService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
